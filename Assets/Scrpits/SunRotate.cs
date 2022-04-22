5cb6f0389efe450a0fe93bf96d2e4d9d30fc712a
Furkan Sert <fssertfs@gmail.com>
2022-04-22 23:43:59 +0300

    sunRotate

--- Assets/Scrpits/SunRotate.cs
+++ Assets/Scrpits/SunRotate.cs
@@ -5,45 +5,34 @@ using UnityEngine;
 public class SunRotate : MonoBehaviour
 {
     bool state1=true, state2;
-    public float maxZ, minZ;
+    public float maxZ, minZInspector;
+    private float minZ;
+    public float _rotationDegree=0;
+    public float _rotationSpeed;
+    public bool rotation = false;
 
     Quaternion target;
     void Start()
     {
+        minZ = 360 + minZInspector;
        
     }
 
     
     void Update()
-    {
-        if (state1&&transform.eulerAngles.z>= maxZ)
+{
+        if(rotation && _rotationDegree < 30f)
         {
-            target = Quaternion.Euler(0, 0, maxZ);
-            state1 = false;
-            state2 = true;
-    
+            _rotationDegree += Time.deltaTime * _rotationSpeed;
+            if (_rotationDegree >= 30f) { _rotationDegree = 30f; rotation = false; }
         }
+        else if(!rotation && _rotationDegree > -30f)
+        {
+            _rotationDegree -= Time.deltaTime * _rotationSpeed;
+            if (_rotationDegree <= -30f) { _rotationDegree = -30f; rotation = true; }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, _rotationDegree);
        
-        else if(state2)
-        {
-            target = Quaternion.Euler(0, 0, minZ);
-     
-        }
-
-        else if (transform.eulerAngles.z<= minZ)
-        {
-            target = Quaternion.Euler(0, 0, minZ);
-            state2 = false;
-            state2 = true;
-
-        }
-
-
-        Debug.Log(transform.eulerAngles.z);
-        
-
-
-        transform.rotation = Quaternion.Lerp(transform.rotation, target, .05f);
         
         
         
