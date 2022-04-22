16e041b36e1971af52b19cf3b079364e8fedec74
Nuh Aydoğdu <nuhaydd25@gmail.com>
2022-04-23 01:09:12 +0300

    scene

---
+++ Assets/Scrpits/PlayerControl.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 2;
+    private float fallMultiplier = 2.5f;
+    private float lowJumpMultiplier = 2f;
+    private float x;
+    public float jumpForce;
+    private bool isGrounded = false;
+    public Transform isGroundedChecker;
+    private float checkGroundRadius = 0.05f;
+    public LayerMask groundLayer;
+    private float rememberGroundedFor = 0.3f;
+    public float lastTimeGrounded;
+    //private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       // animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        CheckIfGrounded();
+        BetterJump();
+        if (x == 0)
+        {
+            //animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            //animator.SetBool("isWalking", true);
+        }
+    }
+    void Move()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = 1;
+            transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            x = -1;
+            transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
+
+
+        }
+        else
+        {
+            x = 0;
+        }
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+
+
+    }
+    void Jump()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor))
+        {
+            //animator.SetBool("isJumping", true);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+    }
+
+
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        
+        if (collider != null)
+        {
+            isGrounded = true;
+            //animator.SetBool("isJumpLoop", false);
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+    void BetterJump()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void JumpLoop()
+    {
+        //animator.SetBool("isJumping", false);
+         //animator.SetBool("isJumpLoop", true);
+    }
+}
+
+
+
